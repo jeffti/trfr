dbac8a56e491340ad32f5204ec4a69427b6010f1
Jefferson <jef_ti@hotmail.com>
2023-12-22 19:03:01 -0300

    New iO

---
+++ io.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var onoff_1 = require("onoff");
+var LED = new onoff_1.Gpio(4, 'out');
+var blinkInterval = setInterval(blinkLED, 250);
+function blinkLED() {
+    if (LED.readSync() === 0) {
+        LED.writeSync(1);
+    }
+    else {
+        LED.writeSync(0);
+    }
+}
+function endBlink() {
+    clearInterval(blinkInterval);
+    LED.writeSync(0);
+    LED.unexport();
+}
+setTimeout(endBlink, 5000);
+//# sourceMappingURL=io.js.map
