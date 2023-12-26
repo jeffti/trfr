edbd440f1c9ffee823f65f894a2189419d3ca923
Jefferson Soares Biernastki <jef_ti@hotmail.com>
2023-12-25 21:24:26 -0300

    Add files via upload

--- io.js
+++ io.js
@@ -1,26 +1,50 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var onoff_1 = require("onoff");
-var LED = new onoff_1.Gpio(4, 'out');
-var blinkInterval = setInterval(blinkLED, 1000);
-var c = 0;
-function blinkLED() {
-    if (LED.readSync() === 0) {
-        LED.writeSync(1);
-    }
-    else {
-        if (c < 5) {
-            c++;
+const axios_1 = __importDefault(require("axios"));
+const blinkInterval = setInterval(checkStatus, 1000);
+const instance = axios_1.default.create({
+    baseURL: 'http://admin.cleean.com.br:8080/api/v1'
+});
+function checkStatus() {
+    instance.get('/status/1', {
+        timeout: 5000
+    })
+        .then(function (response) {
+        if (response.status !== 200) {
+            console.log('Invalid server status', response.status);
             return;
         }
-        LED.writeSync(0);
-        c = 0;
-    }
+        var payload = response.data;
+        if (payload.activate) {
+            console.log('Active server status');
+            setTimeout(resetStatus, 500);
+            console.log('Clearing server status');
+            instance.put('/status/1', {
+                timeout: 5000
+            })
+                .then(function (response) {
+                console.log('Cleared server status', response.data);
+            })
+                .catch(function (reason) {
+                console.log('Failure clearing server status', reason);
+            });
+        }
+        else {
+            console.log('Inactive server status');
+        }
+    })
+        .catch(function (reason) {
+        console.log('Failed requesting server status', reason);
+    });
+    console.log('Requesting server status');
 }
-function endBlink() {
+function resetStatus() {
+    console.log('Reset local status');
+}
+function endOperation() {
     clearInterval(blinkInterval);
-    LED.writeSync(0);
-    LED.unexport();
 }
-setTimeout(endBlink, 1500000);
-//# sourceMappingURL=io.js.map
+setTimeout(endOperation, 1500000);
